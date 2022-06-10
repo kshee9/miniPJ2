fef0ad6bf619634804bae72eada700218d06315f
kshee9 <kiang12@naver.com>
2022-06-10 23:23:30 +0900

    로그인 회원가입 완료

--- src/main/java/com/sparta/page/controller/UserController.java
+++ src/main/java/com/sparta/page/controller/UserController.java
@@ -1,8 +1,48 @@
 package com.sparta.page.controller;
 
-import org.springframework.stereotype.Controller;
+import com.sparta.page.dto.SignupRequestDto;
+import com.sparta.page.dto.UserInfoDto;
+import com.sparta.page.security.UserDetailsImpl;
+import com.sparta.page.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+
+    @Autowired
+    public UserController(UserService userService  ) {
+        this.userService = userService;
+    }
+
+    // 회원 로그인 페이지
+    @GetMapping("/user/loginView")
+    public String login() {
+        return "login";
+    }
+
+    // 회원 가입 페이지
+    @GetMapping("/user/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    // 회원 가입 요청 처리
+    @PostMapping("/user/signup")
+    public void registerUser(@RequestBody SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+    }
+
+    // 회원 관련 정보 받기
+    @PostMapping("/user/userinfo")
+    @ResponseBody
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        String username = userDetails.getUser().getUsername();
+        return new UserInfoDto(username);
+    }
 
 }
