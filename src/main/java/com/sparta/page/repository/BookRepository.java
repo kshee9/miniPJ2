ae588ce620d954b6d252b6207e68b75df45b222d
kshee9 <kiang12@naver.com>
2022-06-11 18:32:09 +0900

    오류로 인한 재커밋

--- src/main/java/com/sparta/page/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.miniteamproject3.repository;
-
-import com.sparta.miniteamproject3.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
